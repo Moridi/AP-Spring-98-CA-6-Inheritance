98f9c0d76f1d157ff32c67017ae79cb534184076
K4mran <kamran2727@gmail.com>
2019-05-10 14:08:25 +0430

    add test6

---
+++ Part-3/testcases/6/main.cpp
@@ -0,0 +1,16 @@
+#include "game_manager.h"
+
+using namespace std;
+
+int main()
+{
+	game_manager game = game_manager();
+	game.add_team_A_goalkeeper(100, 10, 20, 59);
+	game.add_team_A_defender(100, 20, 60, 80);
+	game.add_team_A_striker(100, 90, 50, 60);
+	game.add_team_B_goalkeeper(100, 50, 60, 60);
+	game.add_team_B_defender(100, 60, 70, 80);
+	game.add_team_B_striker(100, 90, 20, 70);
+	game.play();
+	cout << game.get_result();
+}
